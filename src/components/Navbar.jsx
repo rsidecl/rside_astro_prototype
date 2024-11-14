e2719783870968e621a620ccd83c4583697f7d3c
Sam Kee <133723627+samkee10@users.noreply.github.com>
2024-11-14 19:42:35 -0300

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,86 +1,100 @@
-import { useState } from 'react'
-import { motion, AnimatePresence } from 'framer-motion'
-
-const navbarLinks = [{ label: 'R-[ SIDE ]', href: '/', ariaLabel: 'Home' }]
-
-export const Navbar = () => {
-  const [isOpen, setIsOpen] = useState(false)
-
+export const Projects = () => {
   return (
-    <nav className='w-full h-20 flex flex-col justify-center items-center fixed bg-customDarkBg1 lg:bg-customDarkBgTransparent z-40 lg:backdrop-blur-xl'>
-      <div className='2xl:w-[1280px] xl:w-10/12 w-11/12 flex justify-between items-center relative'>
-        <motion.div
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          transition={{ duration: 0.3 }}
-          exit={{ opacity: 0 }}
-        >
-          <a className='navbar-link' href='/' aria-label='Home'>
-            <div className='flex justify-start items-center grow basis-0'>
-              <div className='text-white mr-2 '>
-                <img className='w-32' src='/rsidenb.png' />
-              </div>
-            </div>
-          </a>
-        </motion.div>
-        <motion.div
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          transition={{ duration: 0.3 }}
-          exit={{ opacity: 0 }}
-        >
-          <div className=' lg:flex h-full  mx-2   pl-12 pb-2'>
-            {navbarLinks.map(({ href, label, ariaLabel }) => (
-              <a
-                className='navbar-link text-5xl'
-                href={href}
-                aria-label={ariaLabel}
-                key={label}
-              >
-                {label}
-              </a>
-            ))}
-          </div>
-        </motion.div>
-        <motion.div
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          transition={{ duration: 0.3 }}
-          exit={{ opacity: 0 }}
-        ></motion.div>
-        <div
-          className='hidden flex flex-col  px-2 py-3 border-solid border border-gray-600 rounded-md cursor-pointer hover:bg-customDarkBg2'
-          onClick={() => setIsOpen(!isOpen)}
-        ></div>
+    <section className='flex flex-col lg:flex-row bg-customDarkBg1 mt-10   md:mx-10 lg:mx-32 xl:mx-60'>
+      <div className='flex-1 text-sm sm:text-base mb-6  sm:mt-16 lg:mt-32 mx-auto  font-bold'>
+        <h2 className='text-customSecondary text-2xl  lg:text-2xl'>
+          Proyectos Personalizados para <br /> Impulsar tu Éxito
+        </h2>
+        <h1 className='text-white text-2xl sm:text-3xl lg:text-4xl my-4 lg:my-6'>
+          Cuidamos Cada Detalle <br /> para el Éxito de tu <br /> Negocio
+        </h1>
+        <h3 className='text-gray-400 text-base sm:text-lg lg:text-xl my-4 lg:my-6 leading-relaxed space-y-2'>
+          Ser el aliado estratégico que impulsa <br />
+          el éxito de nuestros clientes, cuidando <br />
+          cada detalle y transformando ideas en <br />
+          soluciones web innovadoras y <br />
+          personalizadas. Nos comprometemos a <br />
+          entender profundamente las <br />
+          necesidades de cada proyecto, creando <br />
+          plataformas dinámicas y efectivas que <br />
+          potencien el crecimiento y la <br />
+          competitividad de cada negocio.
+        </h3>
       </div>
-      <AnimatePresence>
-        {isOpen && (
-          <motion.div
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            transition={{ duration: 0.3 }}
-            exit={{ opacity: 0 }}
-          >
-            <div
-              className='flex flex-col mt-16 lg:hidden absolute top-4 left-0  bg-customDarkBg1 z-50 w-full 
-        items-center gap-10 pb-10 border-y border-solid border-customDarkBg3 pt-10
-        '
-            >
-              {navbarLinks.map(({ label, href, ariaLabel }) => (
-                <a
-                  key={href}
-                  className='navbar-link text-4xl'
-                  href={href}
-                  onClick={() => setIsOpen(false)}
-                  aria-label={ariaLabel}
-                >
-                  {label}
-                </a>
-              ))}
-            </div>
-          </motion.div>
-        )}
-      </AnimatePresence>
-    </nav>
+      <div className='flex-1 mt-6 lg:mt-10 mx-auto rounded-lg'>
+        <div className='flex flex-col md:flex-row items-center md:items-stretch gap-6'>
+          <div className='bg-customDarkBg2 h-auto w-full md:w-72 lg:w-80 py-4 px-4 lg:mb-4 rounded-lg'>
+            <img
+              className='w-16 h-16 lg:w-24 lg:h-24 rounded-lg mx-auto mb-4'
+              src='/assets/icons/settings.svg'
+              alt='Proyecto 1'
+            />
+            <h1 className='text-customSecondary text-lg sm:text-xl mb-4 text-center'>
+              Atención Personalizada y a <br />
+              medida
+            </h1>
+            <p className='text-gray-400 text-sm sm:text-base'>
+              Nos enfocamos en entender y satisfacer las <br />
+              necesidades únicas de cada cliente, brindando <br />
+              un servicio cercano y dedicado a lo largo de <br />
+              todo el proyecto.
+            </p>
+          </div>
+
+          <div className='bg-customDarkBg2 h-auto w-full md:w-72 lg:w-80 py-4 px-4 lg:mb-2 rounded-lg lg:mt-4'>
+            <img
+              className='w-16 h-16 lg:w-24 lg:h-24 rounded-lg mx-auto mb-4'
+              src='/assets/icons/visualcod.svg'
+              alt='Proyecto 2'
+            />
+            <h1 className='text-customSecondary text-lg sm:text-xl mb-4 text-center'>
+              Desarrollo Integral
+            </h1>
+            <p className='text-gray-400 text-sm sm:text-base'>
+              Ofrecemos soluciones completas, desde la <br />
+              concepción de la idea hasta su implementación <br />
+              y mantenimiento, garantizando calidad en <br />
+              cada etapa del proceso.
+            </p>
+          </div>
+        </div>
+
+        <div className='flex flex-col md:flex-row items-center md:items-stretch gap-6 mt-6'>
+          <div className='bg-customDarkBg2 h-auto w-full md:w-72 lg:w-80 py-4 lg:mb-4 px-4 rounded-lg'>
+            <img
+              className='w-16 h-16 lg:w-24 lg:h-24 rounded-lg mx-auto mb-4'
+              src='/assets/icons/screens.svg'
+              alt='Proyecto 3'
+            />
+            <h1 className='text-customSecondary text-lg sm:text-xl mb-4 text-center'>
+              Versatilidad y Adaptabilidad
+            </h1>
+            <p className='text-gray-400 text-sm sm:text-base'>
+              Nos ajustamos a las necesidades de cada <br />
+              proyecto, adaptándonos a diferentes <br />
+              plataformas, tecnologías y estilos para <br />
+              entregar resultados óptimos.
+            </p>
+          </div>
+
+          <div className='bg-customDarkBg2 h-auto w-full md:w-72 lg:w-80 py-4 px-4 mt-6 md:mt-0 rounded-lg lg:mt-4'>
+            <img
+              className='w-16 h-16 lg:w-24 lg:h-24 rounded-lg mx-auto mb-4'
+              src='/assets/icons/pincel.svg'
+              alt='Proyecto 4'
+            />
+            <h1 className='text-customSecondary text-lg sm:text-xl mb-4 text-center'>
+              Diseño Moderno y Dinámico
+            </h1>
+            <p className='text-gray-400 text-sm sm:text-base'>
+              Creamos interfaces atractivas y funcionales que <br />
+              combinan tendencias actuales con elementos <br />
+              interactivos, mejorando la experiencia del <br />
+              usuario en cada visita.
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
   )
 }
