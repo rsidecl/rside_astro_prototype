9b73afdc89a27a00033b4f40eb29a22f622f0a51
Sam Kee <133723627+samkee10@users.noreply.github.com>
2024-11-14 19:40:51 -0300

    Update Companies.jsx

--- src/components/Companies.jsx
+++ src/components/Companies.jsx
@@ -1,46 +1,63 @@
 export const Companies = () => {
   return (
-    <div className='grid grid-cols-2 sm:grid-cols-4 lg:grid-cols-8 gap-6 mt-10 px-4 sm:px-8 md:px-16 lg:px-24'>
-      <img
-        className='w-16 h-auto sm:w-20 md:w-24 lg:w-32  xl:w-36 mx-auto'
-        src='/assets/icons/umce.svg'
-        alt='logo umce'
-      />
-      <img
-        className='w-16 h-auto sm:w-20 md:w-24 lg:w-32 xl:w-36 mx-auto '
-        src='/assets/icons/cinies.svg'
-        alt='logo cinies'
-      />
-      <img
-        className='w-16 h-auto sm:w-20 md:w-24 lg:w-32 xl:w-36 mx-auto'
-        src='/assets/icons/umce.svg'
-        alt='logo umce'
-      />
-      <img
-        className='w-16 h-auto sm:w-20 md:w-24 lg:w-32 xl:w-36 mx-auto'
-        src='/assets/icons/cinies.svg'
-        alt='logo cinies'
-      />
-      <img
-        className='w-16 h-auto sm:w-20 md:w-24 lg:w-32 xl:w-36 mx-auto sm:hidden lg:block'
-        src='/assets/icons/umce.svg'
-        alt='logo umce'
-      />
-      <img
-        className='w-16 h-auto sm:w-20 md:w-24 lg:w-32 xl:w-36 mx-auto sm:hidden lg:block'
-        src='/assets/icons/cinies.svg'
-        alt='logo cinies'
-      />
-      <img
-        className='w-16 h-auto sm:w-20 md:w-24 lg:w-32 xl:w-36 mx-auto sm:hidden lg:block'
-        src='/assets/icons/umce.svg'
-        alt='logo umce'
-      />
-      <img
-        className='w-16 h-auto sm:w-20 md:w-24 lg:w-32 xl:w-36 mx-auto sm:hidden lg:block'
-        src='/assets/icons/cinies.svg'
-        alt='logo cinies'
-      />
+    <div className='grid grid-cols-2 sm:grid-cols-2 lg:grid-cols-8 gap-6 mt-10 px-4 sm:px-8 md:px-16 lg:px-24'>
+      <a href='https://www.umce.cl/'>
+        <img
+          className='w-16 h-auto sm:w-20 md:w-24 lg:w-32  xl:w-36 mx-auto mt-4'
+          src='/assets/icons/umce.svg'
+          alt='logo umce'
+        />
+      </a>
+      <a href='https://www.cinies.cl/'>
+        <img
+          className='w-16 h-auto sm:w-20 md:w-24 lg:w-32 xl:w-36 mx-auto '
+          src='/assets/icons/cinies.svg'
+          alt='logo cinies'
+        />
+      </a>
+      <a href='https://www.umce.cl/'>
+        <img
+          className='w-16 h-auto sm:w-20 md:w-24 lg:w-32 xl:w-36 mx-auto mt-4  hidden lg:block '
+          src='/assets/icons/umce.svg'
+          alt='logo umce'
+        />
+      </a>
+      <a href="'https://www.cinies.cl/">
+        <img
+          className='w-16 h-auto sm:w-20 md:w-24 lg:w-32 xl:w-36 mx-auto  hidden lg:block  '
+          src='/assets/icons/cinies.svg'
+          alt='logo cinies'
+        />
+      </a>
+      <a href='https://www.umce.cl/'>
+        <img
+          className='w-16 h-auto sm:w-20 md:w-24 lg:w-32 xl:w-36 mx-auto mt-4  hidden lg:block '
+          src='/assets/icons/umce.svg'
+          alt='logo umce'
+        />
+      </a>
+      <a href="'https://www.cinies.cl/">
+        <img
+          className='w-16 h-auto sm:w-20 md:w-24 lg:w-32 xl:w-36 mx-auto  hidden lg:block  '
+          src='/assets/icons/cinies.svg'
+          alt='logo cinies'
+        />
+      </a>
+      <a href='https://www.umce.cl/'>
+        <img
+          className='w-16 h-auto sm:w-20 md:w-24 lg:w-32 xl:w-36 mx-auto mt-4  hidden lg:block '
+          src='/assets/icons/umce.svg'
+          alt='logo umce'
+        />
+      </a>
+      <a href="'https://www.cinies.cl/">
+        <img
+          className='w-16 h-auto sm:w-20 md:w-24 lg:w-32 xl:w-36 mx-auto  hidden lg:block  '
+          src='/assets/icons/cinies.svg'
+          alt='logo cinies'
+        />
+      </a>
     </div>
   )
 }
+
